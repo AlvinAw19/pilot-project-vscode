e26c9f983fdb2db7e9383840bde8ed059af40632
AlvinAw9101 <alvin@tyrellsys.com.my>
2025-12-16 11:27:24 +0800

    function 2..to pass the test case

--- app/src/Controller/PagesController.php
+++ app/src/Controller/PagesController.php
@@ -32,6 +32,18 @@ use Cake\View\Exception\MissingTemplateException;
 class PagesController extends AppController
 {
     /**
+     * Before filter callback.
+     *
+     * @param \Cake\Event\EventInterface $event The beforeFilter event.
+     * @return void
+     */
+    public function beforeFilter(\Cake\Event\EventInterface $event)
+    {
+        parent::beforeFilter($event);
+        $this->Authentication->allowUnauthenticated(['display']);
+    }
+    
+    /**
      * Displays a view
      *
      * @param string ...$path Path segments.
